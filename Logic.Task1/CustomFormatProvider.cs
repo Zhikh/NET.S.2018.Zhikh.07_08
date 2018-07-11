01c039780b9ba2b5ab0664abbfeb895f950730d4
Domino <zhikhanastasya@gmail.com>
2018-07-11 16:04:27 +0300

    Add files

---
+++ Logic.Task1/CustomFormatProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace Logic.Task1
+{
+    public sealed class CustomFormatProvider : IFormatProvider, ICustomFormatter
+    {
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            if (arg == null)
+            {
+                throw new ArgumentException();
+            }
+
+            string result;
+
+            IFormattable formattable = arg as IFormattable;
+            if (formattable == null)
+            {
+                return result = arg.ToString();
+            }
+
+            return formattable.ToString(format, formatProvider);
+        }
+
+        public object GetFormat(Type formatType)
+        {
+            if (formatType == typeof(ICustomFormatter))
+            {
+                return this;
+            }
+
+            return Thread.CurrentThread.CurrentCulture.GetFormat(formatType);
+        }
+    }
+}
---
+++ Logic.Task1/Customer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace Logic.Task1
+{
+    public sealed class Customer : IFormattable
+    {
+        public string Name { get; set; }
+
+        public string ContactPhone { get; set; }
+
+        public decimal Revenue { get; set; }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            if (formatProvider != null)
+            {
+                ICustomFormatter formatter =
+                    formatProvider.GetFormat(this.GetType())
+                            as ICustomFormatter;
+                if (formatter != null)
+                {
+                    return formatter.Format(format, this, formatProvider);
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < format.Length; i++)
+            {
+                switch (format[i])
+                {
+                    case 'A':
+                        sb.Append(Name);
+                        break;
+                    case 'B':
+                        sb.Append(ContactPhone);
+                        break;
+                    default:
+                        sb.Append(Revenue.ToString(format[i].ToString(), CultureInfo.InvariantCulture));
+                        break;
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
